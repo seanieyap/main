b0409a3534230835dbb702151dee5a7a2775bd90
Sean Yap <e0177170@u.nus.edu>
2019-02-20 03:12:11 +0800

    SortCommand.java: update to process either "ascending" or "descending"

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -2,21 +2,32 @@ package seedu.addressbook.commands;
 
 
 /**
- * Lists all persons in the address book to the user.
+ * Sorts all people in the address book by name in ascneding/descending lexicographical order.
  */
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
-            + "Sort all persons in the address book in ascending order.\n\t"
+            + "Sort all persons in the address book in lexicographical order.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Address book has been sorted by ascending order!";
+    public static final String MESSAGE_SUCCESS_ASCENDING = "Address book has been sorted in lexicographical order!";
+    public static final String MESSAGE_SUCCESS_DESCENDING = "Address book has been sorted in colexicographical order!";
+    private boolean ascending = true;
+
 
     @Override
     public CommandResult execute() {
-        addressBook.sort();
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (ascending){
+            addressBook.sort();
+            return new CommandResult(MESSAGE_SUCCESS_ASCENDING);
+        }
+        addressBook.sortReverse();
+        return new CommandResult(MESSAGE_SUCCESS_DESCENDING);
+    }
+
+    public void setAscending(boolean ascending) {
+        this.ascending = ascending;
     }
 }
