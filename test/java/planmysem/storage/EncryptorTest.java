94f6fde2db5edbab9560f9c4a798bfe569a945b4
macchazuki <sly_1996@hotmail.com>
2019-03-03 16:16:01 +0800

    added junit class

---
+++ test/java/planmysem/storage/EncryptorTest.java
@@ -0,0 +1,21 @@
+package planmysem.storage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+public class EncryptorTest {
+
+    @Test
+    public void encrypt_Valid() {
+        String testString = Encryptor.encrypt("Hello World!");
+        assertNotEquals(testString, "HellO World!");
+        }
+
+    @Test
+    public void decrypt_Valid() {
+        String testString = Encryptor.encrypt("Hello World!");
+        assertEquals(Encryptor.decrypt(testString), "HellO World!");
+    }
+}
